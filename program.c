a3fec97e0ed832a4e754eebeb2b817ef210c7164
Deynotor <kfy4chnjzm@privaterelay.appleid.com>
2026-09-18 09:51:22 +0000

    changement

--- program.c
+++ program.c
@@ -8,23 +8,22 @@ int main()
 
     /* Anemometre */
     int vitesse = 245;
-    char uniteVitesse[] = "kts";
 
     /* Horizon artificiel */
     int assiette = 5;
     int roulis = 15;
 
     /* Altimetre */
-    int altitude = 35000;
-    char uniteAltitude[] = "ft";
+    int alt = 35000;
+    int ft = 200 ;
 
     /* Variometre */
     int vario = 500;
 
     /* Indicateur directionnel */
     int cap = 72;
-    char direction[] = "ENE";
 
+    int deg = 0;
 
     /* =========================================================
        TABLEAU DE BORD
@@ -37,32 +36,30 @@ int main()
     printf("|    ANEMOMETRE     |     HORIZON ARTIFICIEL     |      ALTIMETRE        |\n");
     printf("|                   |                            |                       |\n");
 
-    printf("|       300         |            30 deg          |       36     37       |\n");
-    printf("|        |          |         \\        /         |          |            |\n");
+    printf("|       %5d       |           30 deg           |       %2d   %2d       |\n",vitesse+55,ft+100,ft+120);
+    printf("|        |          |        \\        /          |          |            |\n");
 
-    printf("|   250--+          |          \\      /          |       35  +---        |\n");
+    printf("|   %5d--+        |         \\      /           |       %2d  +---       |\n",vitesse+5, ft+80);
 
-    printf("|        |          |     ------+------           |          |            |\n");
-    printf("| 200 ---+          |           +                |       %5d %s        |\n",
-           altitude, uniteAltitude);
+    printf("|        |          |      ------+------         |          |            |\n");
+    printf("| %5d ---+        |            +               |       %5d           |\n",vitesse-45, ft+60) ;
 
-    printf("|        |          |          /|\\               |          |            |\n");
+    printf("|        |          |           /|\\              |          |            |\n");
 
-    printf("| 150 ---+          |       --- + ---             |       34  +---        |\n");
+    printf("| %5d ---+        |        --- + ---           |       %2d +---        |\n",vitesse-95, ft+40);
 
-    printf("|        |          |           |                |          |            |\n");
+    printf("|        |          |            |               |          |            |\n");
 
-    printf("| 100 ---+          |        ASSIETTE            |          |            |\n");
+    printf("| %5d ---+        |         ASSIETTE           |           %2d         |\n",vitesse-145, ft+20);
     printf("|        |          |          %+d deg            |          |            |\n",
            assiette);
 
-    printf("|  50 ---+          |         ROULIS             |       VARIOMETRE       |\n");
+    printf("|  %5d ---+       |          ROULIS            |       VARIOMETRE      |\n", vitesse-195);
 
-    printf("|        |          |          %+d deg            |        %+d ft/min      |\n",
+    printf("|        |          |          %+d deg           |        %+d ft/min    |\n",
            roulis, vario);
 
-    printf("|       %3d %s       |                            |                       |\n",
-           vitesse, uniteVitesse);
+    printf("|     %5d         |                            |                       |\n",vitesse-245) ;
 
     printf("|                   |                            |                       |\n");
     printf("===========================================================================\n");
@@ -71,20 +68,19 @@ int main()
        INDICATEUR DIRECTIONNEL
        ========================================================= */
 
-    printf("|                    INDICATEUR DIRECTIONNEL                            |\n");
-    printf("|                                                                       |\n");
-    printf("|                 N                         0 deg                       |\n");
-    printf("|                 |                           |                         |\n");
-    printf("|            NO ---+--- NE               315 + 45                      |\n");
-    printf("|                 |                           |                         |\n");
-    printf("|            O ---+--- E                270 + 90                      |\n");
-    printf("|                 |                           |                         |\n");
-    printf("|            SO ---+--- SE               225 + 135                    |\n");
-    printf("|                 |                           |                         |\n");
-    printf("|                 S                         180 deg                     |\n");
-    printf("|                                                                       |\n");
-    printf("|                         CAP : %03d deg - %s                         |\n",
-           cap, direction);
+    printf("|                    INDICATEUR DIRECTIONNEL                              |\n");
+    printf("|                                                                         |\n");
+    printf("|                 N                          deg                          |\n");
+    printf("|                 |                           |                           |\n");
+    printf("|           NO ---+--- NE                     %2d                          |\n", deg+30);
+    printf("|                 |                           |                           |\n");
+    printf("|            O ---+--- E                      %2d                          |\n", deg+20);
+    printf("|                 |                           |                           |\n");
+    printf("|           SO ---+--- SE                     %2d                          |\n", deg+10);
+    printf("|                 |                           |                           |\n");
+    printf("|                 S                         %2d deg                        |\n", deg+0 );
+    printf("|                                                                         |\n");
+    printf("|                         CAP : %03d deg -                                 |\n",cap+0);
     printf("===========================================================================\n");
 
     return 0;
